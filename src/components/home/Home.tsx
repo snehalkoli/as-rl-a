ef13e3977460edb205217a782a32472573c7470c
snehalkoli <snehalkoli1001@gmail.com>
2023-01-09 06:21:43 +0530

    initial commit

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,35 @@
+import {useState} from 'react'
+//import AppContext from '../../store/appContext';
+import Navbar from '../navbar/Navbar';
+import AddPeople from '../sections/addPeople/AddPeople';
+import AddRelationship from '../sections/addRelationship/AddRelationship';
+import { Front } from '../sections/front/Front';
+import Separation from '../sections/separation/Separation';
+//import Alerts from '../ui/Alert';
+
+
+const Home:React.FC = () => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const toggle:any = () => {
+        setIsOpen(!isOpen);
+    }
+    // const appCtx = useContext(AppContext);
+    return (
+        <>
+            {/* {appCtx.alert.hasAlert && (
+                <Alerts
+                variant={appCtx.alert.variant}
+                description={appCtx.alert.description}
+                />
+            )} */}
+            <Navbar toggle={toggle} />
+            <Front />
+            <AddPeople />
+            <AddRelationship />
+            <Separation />
+            
+        </>
+    )
+}
+
+export default Home
